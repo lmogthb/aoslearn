97b44f7d1fd39793ca12472d0aed1ecf1b244cae
Alejandro <alejandroolmossanchez02@gmail.com>
2021-12-12 16:20:23 +0100

    register page changed

--- database/migrations/2021_12_09_180506_anadir_role_campo.php
+++ database/migrations/2021_12_09_180506_anadir_role_campo.php
@@ -14,7 +14,7 @@ class AnadirRoleCampo extends Migration
     public function up()
     {
         Schema::table('users', function ($table) {
-            $table->string('rol')->nullable();
+            $table->string('rol')->default('user');;
         });
     }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,60 @@
-@extends('layouts.app')
-
+@extends('layouts.master')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/stylelogin.css') }}">
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="card card0 border-0">
+    <div class="row d-flex">
+        <div class="col-lg-6">
+            <div class="card2 card border-0 px-4 py-5">
+                <h3 class="mb-1">Crear una cuenta en AOSLearn</h3>
+                <p class="font-weight-bold">¿Ya tiene una cuenta? <a class="text-danger " href="{{ route('login') }}">Iniciar sesion</a></p>
+                <form action="{{ route('register') }}" method="post">
+                @csrf
+                    <div class="row px-3"> 
+                        <label class="mb-0">
+                            <h6 class="mb-0 text-sm">Nombre</h6>
+                        </label> 
+                    <input type="text" name="name"> 
+                    @if($errors->has('name'))
+                            <span class="text-danger">{{ $errors->first('name') }}</span>
+                    @endif
+                    </div>
+                    <br>
+                    <div class="row px-3"> 
+                        <label class="mb-0">
+                            <h6 class="mb-0 text-sm">Correo</h6>
+                        </label> 
+                    <input type="text" name="email"> 
+                    @if($errors->has('email'))
+                            <span class="text-danger">{{ $errors->first('email') }}</span>
+                    @endif
+                    </div>
+                    <br>
+                    <div class="row px-3"> 
+                        <label class="mb-0">
+                            <h6 class="mb-0 text-sm">Contraseña</h6>
+                        </label> 
+                        <input type="password" name="password"> 
+                        @if($errors->has('password'))
+                            <span class="text-danger">{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>
+                    <br>
+                    <div class="row px-3"> 
+                        <label class="mb-0">
+                            <h6 class="mb-0 text-sm">Confirmar contraseña</h6>
+                        </label> 
+                        <input type="password" name="password_confirmation">
+                    </div>
+                    <br>
+                    <div class="row mb-4">
+                        <div class="col-md-5"> <button type="submit" class="btn btn-blue text-center mb-1">Crear cuenta</button> </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-md-5"> <a class="btn btn-green text-center mb-1" href="{{ route('recurso.index') }}">Volver atras</a> </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
-@endsection
+@stop
